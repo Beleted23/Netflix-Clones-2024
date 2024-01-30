962d95c222ea50df759476e10593a22614a859bb
Beleted23 <duchishobelete1@gmail.com>
2024-01-29 19:06:14 -0800

    axious and requests

--- netflix-clone/src/Components/Footer/Footer.js
+++ netflix-clone/src/Components/Footer/Footer.js
@@ -2,59 +2,67 @@ import React from 'react';
 import FacebookOutlinedIcon from '@mui/icons-material/Facebook';
 import InstagramIcon from '@mui/icons-material/Instagram';
 import YouTubeIcon from '@mui/icons-material/YouTube';
+import TwitterIcon from '@mui/icons-material/Twitter';
+
 import './Footer.css';
 
 const Footer = () => {
   return (
-    <div className='footer_outer_container'>
-      <div className='footer_inner_container'>
-        <div className='header_container'>
-          <div className='footer_Icons'> 
-            <FacebookOutlinedIcon />
-            <InstagramIcon />
-            <YouTubeIcon />
-          </div>
-          <div className='footer_data'>
-            <ul>
-              <li>Audio Discription</li>
-              <li>Investor Relations</li>
-              <li>Legal Notice</li>
-            </ul>
-          </div>
-          <div className='footer_data'>
-            <ul>
-              <li>Help Center</li>
-              <li>Jobs</li>
-              <li>Cookie Preferences</li>
-            </ul>
-          </div>
-          <div className='footer_data'>
-            <ul>
-              <li>Gift Cards</li>
-              <li>Terms of Use</li>
-              <li>Corporate information</li>
-            </ul>
-          </div>
-          <div className='footer_data'>
-            <ul>
-              <li>Media Content</li>
-              <li>Privacy</li>
-              <li>Contact Us</li>
-            </ul>
-          </div>
-          </div>
-          <div className='Service_code'>
-            <p>
+    <div className="footer_outer_container">
+    <div className="footer_inner_container">
+      <div className="footer_icons">
+        <FacebookOutlinedIcon />
+        <InstagramIcon />
+        <TwitterIcon/>
+        <YouTubeIcon />
+      </div>
+      <div className="foooter_data">
+        <div>
+          <ul>
+            <li>Audio Description</li>
+            <li>Investor Relations</li>
+            <li> Privacy</li>
+            <li>Contact us</li>
+            <div className=""></div>
+          
+          </ul>
+        </div>
+        <div>
+          <ul>
+            <li>Help Center</li>
+            <li>Jobs</li>
+            <li>Legal Notices</li>
+            <li>Do Not Sell or Share My Personal Information</li>
+          </ul>
+        </div>
 
-             Service code
-            </p>
-          </div>
-          <div className='Copy_write' >
-            &Copy; 1997-2024 Netflix, Inc
+        <div>
+          <ul>
+            <li>Git Card</li>
+            <li>Netflix shop</li>
+            <li>Cookies Preferences</li>
+            <li>Ad Choices</li>
+          </ul>
+        </div>
+
+        <div>
+          <ul>
+            <li>Media Center</li>
+            <li>Terms of Use</li>
+            <li>Corporate Information</li>
+          </ul>
         </div>
       </div>
+      <div className="service_code">
+        <p>service code</p>
+      </div>
+      <div className="copy-write">&copy; 1997-2024 Netflix. inc.</div>
     </div>
-  );
+    </div>
+  );
+
+    
+  
 }
 
 export default Footer;
---
+++ netflix-clone/src/Utils/axios.js
@@ -0,0 +1,13 @@
+
+import axios from 'axios';
+
+
+const instance = "axios".create({
+    baseurl: "https://api.themoviedb.org/3/",
+});
+
+
+export default instance;
+
+
+
