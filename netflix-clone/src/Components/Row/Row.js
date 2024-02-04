cc771ecaf48f1f3fefa0426d6501980e417b905f
Beleted23 <duchishobelete1@gmail.com>
2024-02-03 22:06:26 -0800

    deployed

--- netflix-clone/src/Components/Row/Row.js
+++ netflix-clone/src/Components/Row/Row.js
@@ -14,9 +14,9 @@ const Row = ({ title, fetchUrl, isLargeRow }) => {
     useEffect(() => {
         (async () => {
             try {
-                // console.log(fetchUrl)
+                
                 const request = await axios.get(fetchUrl);
-                // console.log(request)
+                
                 setMovie(request.data.results);
             } catch (error) {
                 console.log("error", error);
